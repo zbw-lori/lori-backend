4a47b6448e728bc77f3b410d5c4d5cc2806a8dc3
faesslerm <markus.mfs@gmail.com>
2023-02-04 10:21:32 +0100

    Add simple backend

---
+++ src/Controllers/MessageController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sample.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly MqttClient _client;
+        public MessageController()
+        {
+            _client = new MqttClient();
+        }
+
+        ~MessageController()
+        {
+            _client.Disconnect();
+        }
+
+        // GET: api/<MessageController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MessageController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MessageController>
+        [HttpPost]
+        public async Task Post([FromBody] string value)
+        {
+            Console.WriteLine($"Received message: {value}");
+            Console.WriteLine("Publish message with topic 'test' on mqtt...");
+
+            await _client.Connect();
+            await _client.Publish("test", value);
+            await _client.Subscribe("test/response");
+            Console.WriteLine("Done");
+        }
+
+        // PUT api/<MessageController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MessageController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/Services/MqttClient.cs
@@ -0,0 +1,71 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+
+namespace Sample.Services
+{
+    public class MqttClient
+    {
+        private readonly MqttFactory _factory;
+        private readonly IMqttClient _client;
+        private readonly string _clientId = "backend";
+        private readonly string _server = "localhost";
+        private readonly int _port = 1883;
+        public MqttClient()
+        {
+            _factory = new MqttFactory();
+            _client = _factory.CreateMqttClient();
+        }
+
+        public async Task Connect()
+        {
+            if (_client.IsConnected)
+            {
+                return;
+            }
+
+            var options = new MqttClientOptionsBuilder()
+                    .WithClientId(_clientId)
+                    .WithTcpServer(_server, _port)
+                    .Build();
+
+            await _client.ConnectAsync(options, CancellationToken.None);
+        }
+
+        public void Disconnect()
+        {
+            //await _client.DisconnectAsync(); //todo: do we need to call disconnect?
+            _client.Dispose();
+        }
+
+        public async Task Publish(string topic, string message)
+        {
+            var applicationMessage = new MqttApplicationMessageBuilder()
+                    .WithTopic(topic)
+                    .WithPayload(message)
+                    .Build();
+
+            await _client.PublishAsync(applicationMessage, CancellationToken.None);
+        }
+
+        public async Task Subscribe(string topic)
+        {
+            var subscribeOptions = _factory.CreateSubscribeOptionsBuilder()
+                    .WithTopicFilter(
+                        f =>
+                        {
+                            f.WithTopic($"{topic}");
+                        })
+                    .Build();
+
+            var response = await _client.SubscribeAsync(subscribeOptions, CancellationToken.None);
+            _client.ApplicationMessageReceivedAsync += e =>
+            {
+                var message = System.Text.Encoding.Default.GetString(e.ApplicationMessage.Payload);
+                Console.WriteLine(message);
+                return Task.CompletedTask;
+            };
+
+            Console.WriteLine("MQTT client subscribed to topic.");
+        }
+    }
+}
