afd308205e656d7c23348376bf71f4dd271f2788
Fabian Dort <22558338+dortlii@users.noreply.github.com>
2023-03-30 22:27:29 +0200

    feat: added Model for customer table

---
+++ src/lori.backend.Infrastructure/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace lori.backend.Infrastructure.Models
+{
+  public class Customer
+  {
+    public int Id { get; set; }
+    public string Forename { get; set; }
+    public string Surename { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public int AddressId { get; set; }
+  }
+}
+
