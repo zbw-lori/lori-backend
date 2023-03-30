86a27882893c98ab5e68957225a17177b0d5fba8
dortlii <22558338+dortlii@users.noreply.github.com>
2023-03-30 23:26:29 +0200

    feat: added table route

---
+++ src/lori.backend.Infrastructure/Models/Route.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace lori.backend.Infrastructure.Models;
+public class Route
+{
+  public int Id { get; set; }
+  [Column(TypeName = "json")]
+  public string PriorityList { get; set; } = null!;
+}
