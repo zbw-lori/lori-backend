0623329c712b537aec2173a176194380341eb1d1
dortlii <22558338+dortlii@users.noreply.github.com>
2023-03-30 23:26:13 +0200

    feat: added table order

---
+++ src/lori.backend.Infrastructure/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lori.backend.Infrastructure.Models;
+public class Order
+{
+  public int Id { get; set; }
+  public string Status { get; set; } = null!;
+  public int Priority { get; set; }
+  public DateTime dateTime { get; set; }
+  public string ReceiptType { get; set;} = null!;
+  public int CustomerId { get; set; }
+}
