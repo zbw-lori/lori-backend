9f9e0b9a804971b01ebef39b9587d8e9dfc15940
dortlii <22558338+dortlii@users.noreply.github.com>
2023-03-30 23:26:08 +0200

    feat: added table login

---
+++ src/lori.backend.Infrastructure/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lori.backend.Infrastructure.Models;
+public class Login
+{
+  [Key]
+  public string Username { get; set; } = null!;
+  [Key]
+  public Byte[] Password { get; set; } = null!;
+}
