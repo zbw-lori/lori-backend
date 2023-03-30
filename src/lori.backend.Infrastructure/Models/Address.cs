d701c7aabf393e5fe86423c678b9f26c889d7f41
dortlii <22558338+dortlii@users.noreply.github.com>
2023-03-30 23:25:50 +0200

    feat: added table address

--- src/lori.backend.Infrastructure/Models/Address.cs
+++ src/lori.backend.Infrastructure/Models/Address.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-namespace lori.backend.Infrastructure.Models
+namespace lori.backend.Infrastructure.Models;
+
+public class Address
 {
-  public class Address
-  {
-    public int Id { get; set; }
-    public string Street { get; set; }
-    public int StreetNumber { get; set; }
-    public string City { get; set; }
-    public int CityCode { get; set; }
-  }
+  public int Id { get; set; }
+  public string Street { get; set; } = null!;
+  public int StreetNumber { get; set; }
+  public string City { get; set; } = null!;
+  public int CityCode { get; set; }
 }
 
