36f624bd36d68ff9a0b0ab08503643bba1c81f47
dortlii <22558338+dortlii@users.noreply.github.com>
2023-03-30 23:26:35 +0200

    feat: added table store

---
+++ src/lori.backend.Infrastructure/Models/Store.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lori.backend.Infrastructure.Models;
+public class Store
+{
+  public int Id { get; set; }
+  public string Name { get; set; } = null!;
+  public int Quantity { get; set; }
+}
