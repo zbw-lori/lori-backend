027dfff40d0f138fb6d3dc58eae023880d4b4bdb
dortlii <22558338+dortlii@users.noreply.github.com>
2023-03-30 23:26:19 +0200

    feat: added table orderitem

---
+++ src/lori.backend.Infrastructure/Models/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lori.backend.Infrastructure.Models;
+public class OrderItem
+{
+  public int OrderId { get; set; }
+  public int ItemId { get; set; }
+  public int Quantity { get; set; }
+}
