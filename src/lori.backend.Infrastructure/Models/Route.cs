33a5c337f924d42a7457a23230e7cb9d537ed27b
dortlii <22558338+dortlii@users.noreply.github.com>
2023-04-05 19:55:35 +0200

    feat(Route): added foreign keys

--- src/lori.backend.Infrastructure/Models/Route.cs
+++ src/lori.backend.Infrastructure/Models/Route.cs
@@ -12,4 +12,10 @@ public class Route
   public int Id { get; set; }
   [Column(TypeName = "json")]
   public string PriorityList { get; set; } = null!;
+
+  public int RobotId { get; set; }
+  public Robot Robot { get; set; } = null!;
+
+  public int OrderId { get; set; }
+  public Order Order { get; set; } = null!;
 }
