cd4c1089d88b37bf44414d326ae60dace9b37b5f
dortlii <22558338+dortlii@users.noreply.github.com>
2023-04-06 23:47:15 +0200

    feat: added GetRobots and GetRobot methods

---
+++ src/lori.backend.Web/Api/v1/RobotController.cs
@@ -0,0 +1,54 @@
+﻿using lori.backend.Infrastructure.Data;
+using lori.backend.Infrastructure.Models;
+using lori.backend.Web.ApiModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace lori.backend.Web.Api.v1;
+[Route ("api/v1/[controller]")]
+[ApiController]
+public class RobotController : ControllerBase
+{
+  private readonly LoriDbContext _context;
+
+  public RobotController(LoriDbContext context)
+  {
+    _context = context;
+  }
+
+  // GET: api/v1/robot
+  [HttpGet]
+  public async Task<ActionResult<IEnumerable<RobotDTO>>> GetRobots()
+  {
+    return await _context.Robots
+      .Select(x => RobotToDTO(x))
+      .ToListAsync();
+  }
+
+  // GET: api/v1/robot/1
+  // <snippet_GetByID>
+  [HttpGet("{id}")]
+  public async Task<ActionResult<RobotDTO>> GetRobot(int id)
+  {
+    var robot = await _context.Robots.FindAsync(id);
+
+    if (robot == null)
+    {
+      return NotFound();
+    }
+
+    return RobotToDTO(robot);
+  }
+
+  // PUT: api/v1/robot/1
+  // <snippet_Update>
+
+  private static RobotDTO RobotToDTO(Robot robot) =>
+    new RobotDTO
+    {
+      Id = robot.Id,
+      Name = robot.Name,
+      Description = robot.Description,
+      Model = robot.Model
+    };
+}
