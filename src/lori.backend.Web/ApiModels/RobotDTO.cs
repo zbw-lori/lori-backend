856f332b57193ad7bf4d078f229583b164eee0cd
dortlii <22558338+dortlii@users.noreply.github.com>
2023-04-06 23:46:42 +0200

    feat: added robo dto model

---
+++ src/lori.backend.Web/ApiModels/RobotDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace lori.backend.Web.ApiModels;
+
+public class RobotDTO
+{
+  public int Id { get; set; }
+  public string Name { get; set; } = null!;
+  public string Description { get; set; } = null!;
+  public string Model { get; set; } = null!;
+}
