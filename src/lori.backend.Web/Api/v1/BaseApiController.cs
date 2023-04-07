7759f08615333424da7edd024556c9cf1a545526
dortlii <22558338+dortlii@users.noreply.github.com>
2023-04-07 21:30:00 +0200

    feat: added basecontroller

---
+++ src/lori.backend.Web/Api/v1/BaseApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lori.backend.Web.Api.v1;
+
+/// <summary>
+/// If your API controllers will use a consistent route convention and the [ApiController] attribute (they should)
+/// then it's a good idea to define and use a common base controller class like this one.
+/// </summary>
+[Route("api/v1")]
+[ApiController]
+public abstract class BaseApiController : Controller
+{
+}
