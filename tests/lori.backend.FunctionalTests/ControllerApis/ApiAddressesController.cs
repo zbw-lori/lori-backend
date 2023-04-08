754112b479930cb565c929bd372f8e35cbf08bbf
faesslerm <markus.mfs@gmail.com>
2023-04-08 13:18:26 +0200

    feat: add funtional tests of AddressController

---
+++ tests/lori.backend.FunctionalTests/ControllerApis/ApiAddressesController.cs
@@ -0,0 +1,30 @@
+﻿using Ardalis.HttpClientTestExtensions;
+using lori.backend.Infrastructure.Models;
+using Xunit;
+
+namespace lori.backend.FunctionalTests.ControllerApis;
+
+[Collection("Sequential")]
+public class ApiAddressesController : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+  private readonly HttpClient _client;
+
+  public ApiAddressesController(CustomWebApplicationFactory<Program> factory)
+  {
+    _client = factory.CreateClient();
+  }
+
+  [Fact]
+  public async Task ReturnsSeedProjectGivenId1()
+  {
+    var result = await _client.GetAndDeserializeAsync<IEnumerable<Address>>("/api/v1/address");
+
+    Assert.Equal(2, result.Count());
+  }
+
+  [Fact]
+  public async Task ReturnsNotFoundGivenId0()
+  {
+    await _client.GetAndEnsureNotFoundAsync("/api/v1/address/0");
+  }
+}
