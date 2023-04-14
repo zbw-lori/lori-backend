e44b3d288efe23e12e759d559d9ca1442b3a47e7
faesslerm <markus.mfs@gmail.com>
2023-04-14 18:55:26 +0200

    feat: added role model

---
+++ src/lori.backend.Infrastructure/Models/Role.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lori.backend.Infrastructure.Models;
+public class Role
+{
+  [Key]
+  public int Id { get; set; }
+  public string Name { get; set; } = null!;
+  public string Description { get; set; } = null!;
+}
