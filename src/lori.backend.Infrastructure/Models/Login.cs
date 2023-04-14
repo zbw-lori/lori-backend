a76f4d76d88aa170076b58b4a942a9d939ba3f9c
faesslerm <markus.mfs@gmail.com>
2023-04-14 18:56:53 +0200

    feat: added role property to login model

--- src/lori.backend.Infrastructure/Models/Login.cs
+++ src/lori.backend.Infrastructure/Models/Login.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace lori.backend.Infrastructure.Models;
 public class Login
@@ -11,4 +6,6 @@ public class Login
   [Key]
   public string Username { get; set; } = null!;
   public Byte[] Password { get; set; } = null!;
+  public int RoleId { get; set; }
+  public Role? Role { get; set; }
 }
