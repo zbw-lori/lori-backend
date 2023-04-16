9dd7dafb2d071340f122734960328c5c71e99865
faesslerm <markus.mfs@gmail.com>
2023-04-16 07:14:30 +0200

    feat: activate authentication

--- src/lori.backend.Web/Program.cs
+++ src/lori.backend.Web/Program.cs
@@ -2,9 +2,14 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using lori.backend.Core;
+using lori.backend.Core.Interfaces;
 using lori.backend.Infrastructure;
+using lori.backend.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Serilog;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,18 +23,45 @@ builder.Services.Configure<CookiePolicyOptions>(options =>
   options.MinimumSameSitePolicy = SameSiteMode.None;
 });
 
-string connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+string? connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
 
-builder.Services.AddDbContext(connectionString);
+builder.Services.AddDbContext(connectionString!);
 
-builder.Services.AddControllersWithViews().AddNewtonsoftJson();
-builder.Services.AddRazorPages();
+builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<ILoginService, LoginService>();
+builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddSwaggerGen(c =>
 {
   c.SwaggerDoc("v1", new OpenApiInfo { Title = "LORI API", Version = "v1" });
   c.EnableAnnotations();
+
+  c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+  {
+    Description = "Standard Authorization header using the Bearer scheme",
+    In = ParameterLocation.Header,
+    Name = "Authorization",
+    Type = SecuritySchemeType.Http,
+    BearerFormat = "JWT",
+    Scheme = "Bearer"
+  });
+  c.OperationFilter<SecurityRequirementsOperationFilter>();
 });
+
+var secret = builder.Configuration.GetValue<string>("AppSettings:Token");
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+    {
+      ClockSkew = TimeSpan.Zero,
+      ValidateIssuer = true,
+      ValidateAudience = true,
+      ValidateLifetime = true,
+      ValidateIssuerSigningKey = true,
+      ValidIssuer = builder.Configuration.GetValue<string>("AppSettings:Issuer"),
+      ValidAudience = builder.Configuration.GetValue<string>("AppSettings:Audience"),
+      IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(secret!)),
+    });
 
 // add list services for diagnostic purposes - see https://github.com/ardalis/AspNetCoreStartupServices
 builder.Services.Configure<ServiceConfig>(config =>
@@ -66,6 +98,8 @@ app.UseRouting();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseCookiePolicy();
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Enable middleware to serve generated Swagger as a JSON endpoint.
 app.UseSwagger();
