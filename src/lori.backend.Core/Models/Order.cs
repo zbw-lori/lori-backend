9e69d40795b6c071be944cc8a8857217dcefb348
dortlii <22558338+dortlii@users.noreply.github.com>
2023-04-18 20:04:00 +0200

    feat: renamed column to "Created"

--- src/lori.backend.Core/Models/Order.cs
+++ src/lori.backend.Core/Models/Order.cs
@@ -4,7 +4,7 @@ public class Order
   public int Id { get; set; }
   public string Status { get; set; } = null!;
   public int Priority { get; set; }
-  public DateTime dateTime { get; set; }
+  public DateTime Created { get; set; }
   public string ReceiptType { get; set;} = null!;
   public int CustomerId { get; set; }
   public Customer Customer { get; set; } = null!;
