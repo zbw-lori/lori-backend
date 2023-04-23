4cdd4b9e4b607814911486b17eef03f9e38ee844
faesslerm <markus.mfs@gmail.com>
2023-04-23 09:18:46 +0200

    feat: modify mqtt service interface

--- src/lori.backend.Core/Interfaces/IMqttService.cs
+++ src/lori.backend.Core/Interfaces/IMqttService.cs
@@ -1,9 +1,11 @@
-﻿namespace lori.backend.Core.Interfaces;
+﻿using MQTTnet.Client;
+
+namespace lori.backend.Core.Interfaces;
 public interface IMqttService
 {
   void Connect();
   void Disconnect();
   void Publish(string topic, string data);
-  void Subscribe(string topic, string data);
-  void Unsubscribe(string topic, string data);
+  void Subscribe(string topic, Func<MqttApplicationMessageReceivedEventArgs, Task> callback);
+  void Unsubscribe(string topic);
 }
