79283cf95badc7cefa62364db55f8e47606f4a60
faesslerm <markus.mfs@gmail.com>
2023-04-23 08:50:15 +0200

    feat: add interface of mqtt service

---
+++ src/lori.backend.Core/Interfaces/IMqttService.cs
@@ -0,0 +1,9 @@
+﻿namespace lori.backend.Core.Interfaces;
+public interface IMqttService
+{
+  void Connect();
+  void Disconnect();
+  void Publish(string topic, string data);
+  void Subscribe(string topic, string data);
+  void Unsubscribe(string topic, string data);
+}
