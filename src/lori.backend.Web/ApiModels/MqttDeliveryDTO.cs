bd7fd51fd7b292a65a89e2ae4306fe4957d7b90e
faesslerm <markus.mfs@gmail.com>
2023-04-24 18:48:21 +0200

    feat: add delivery dto

---
+++ src/lori.backend.Web/ApiModels/MqttDeliveryDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace lori.backend.Web.ApiModels;
+
+public class MqttDeliveryDTO
+{
+  public int OrderId { get; set; }
+  public string Product { get; set; }
+  public string DeliveryType { get; set; }
+}
