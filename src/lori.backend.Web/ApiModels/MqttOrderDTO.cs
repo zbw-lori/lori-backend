42d9ec76a43c464742bd92b2d6f9c56f2f76e543
faesslerm <markus.mfs@gmail.com>
2023-04-24 18:48:12 +0200

    feat: add order dto

---
+++ src/lori.backend.Web/ApiModels/MqttOrderDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace lori.backend.Web.ApiModels;
+
+public class MqttOrderDTO
+{
+  public int QuantityItem1 { get; set; }
+  public int QuantityItem2 { get; set; }
+  public int QuantityItem3 { get; set; }
+  public int QuantityItem4 { get; set; }
+}
