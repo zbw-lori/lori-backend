1ad4cfd81c171e1c4d29e0f21f31aa9dd4ee7716
Fabian Dort <22558338+dortlii@users.noreply.github.com>
2023-05-08 22:40:03 +0200

    feat: added StockQuantity property

--- src/lori.backend.Core/Models/Item.cs
+++ src/lori.backend.Core/Models/Item.cs
@@ -4,6 +4,7 @@ public class Item
   public int Id { get; set; }
   public string Name { get; set; } = null!;
   public decimal Price { get; set; }
+  public int StockQuantity { get; set; }
   public int StoreId { get; set; }
   public Store Store { get; set; } = null!;
 }
