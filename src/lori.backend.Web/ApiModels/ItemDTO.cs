a2aec10bf3ecb3279d213aaac421c332d1405c8a
Fabian Dort <22558338+dortlii@users.noreply.github.com>
2023-05-19 20:41:40 +0200

    feat: added new property StockQuantity

--- src/lori.backend.Web/ApiModels/ItemDTO.cs
+++ src/lori.backend.Web/ApiModels/ItemDTO.cs
@@ -5,5 +5,6 @@ public class ItemDTO
   public int Id { get; set; }
   public string Name { get; set; } = null!;
   public decimal Price { get; set; }
+  public int StockQuantity { get; set; }
   public int StoreId { get; set; }
 }
