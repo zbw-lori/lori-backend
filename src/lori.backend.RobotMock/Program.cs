e6e3764138182e3c22c831eb64e9ce33770b367c
faesslerm <markus.mfs@gmail.com>
2023-05-20 15:19:38 +0200

    feat: add project reference to lori.backend.Infrastructure

--- src/lori.backend.RobotMock/Program.cs
+++ src/lori.backend.RobotMock/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace lori.backend.RobotMock;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    Console.WriteLine("Hello, blah!");
+  }
+}
