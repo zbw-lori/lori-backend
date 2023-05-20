1d41a996d6c8391ad2cbe10a4d499c03f063218e
faesslerm <markus.mfs@gmail.com>
2023-05-20 07:24:30 +0200

    feat: add location to mqtt delivery dto

--- src/lori.backend.Web/ApiModels/MqttDeliveryDTO.cs
+++ src/lori.backend.Web/ApiModels/MqttDeliveryDTO.cs
@@ -3,6 +3,7 @@
 public class MqttDeliveryDTO
 {
   public int OrderId { get; set; }
-  public string Product { get; set; }
-  public string DeliveryType { get; set; }
+  public int Location { get; set; }
+  public string Product { get; set; } = null!;
+  public string DeliveryType { get; set; } = null!;
 }
