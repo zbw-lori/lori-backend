4bdc76c1b89dde8b91aeae80b4a88a8ab2c68030
dortlii <22558338+dortlii@users.noreply.github.com>
2023-05-30 13:31:54 +0200

    feat: added new Order properties

--- src/lori.backend.Core/Models/Order.cs
+++ src/lori.backend.Core/Models/Order.cs
@@ -4,7 +4,8 @@ public class Order
   public int Id { get; set; }
   public string Status { get; set; } = null!;
   public Priority Priority { get; set; }
-  public DateTime Created { get; set; }
+  public DateTime EarliestDelivery { get; set; }
+  public DateTime LatestDelivery { get; set; }
   public ReceiptType ReceiptType { get; set; }
   public int CustomerId { get; set; }
   public Customer Customer { get; set; } = null!;
