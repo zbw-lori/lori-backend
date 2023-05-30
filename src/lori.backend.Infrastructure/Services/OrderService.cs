893c7b6d07f42ff38dee9f00667b86f6f51b83e8
dortlii <22558338+dortlii@users.noreply.github.com>
2023-05-30 14:09:12 +0200

    feat: added method GetSortedOrders

---
+++ src/lori.backend.Infrastructure/Services/OrderService.cs
@@ -0,0 +1,23 @@
+﻿namespace lori.backend.Core.Services;
+
+using lori.backend.Core.Models;
+using lori.backend.Infrastructure.Data;
+public class OrderService
+{
+  private readonly LoriDbContext _context;
+
+  public OrderService(LoriDbContext context)
+  {
+    _context = context;
+  }
+
+  public List<Order> GetSortedOrders()
+  {
+    var sortedOrders = _context.Orders
+        .OrderBy(o => o.Priority)
+        .ThenBy(o => o.EarliestDelivery)
+        .ToList();
+
+    return sortedOrders;
+  }
+}
