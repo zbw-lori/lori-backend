49374b43d5262bb6d2d9ab2f7abf35c928381ae2
faesslerm <markus.mfs@gmail.com>
2023-05-31 17:14:11 +0200

    feat: implement simple robot mocking

--- src/lori.backend.RobotMock/Program.cs
+++ src/lori.backend.RobotMock/Program.cs
@@ -1,10 +1,56 @@
-﻿
+﻿using System.Text.Json;
+using lori.backend.Core.Interfaces;
+using lori.backend.Infrastructure.Services;
+
 namespace lori.backend.RobotMock;
 
 public class Program
 {
-  public static void Main(string[] args)
+  private static readonly string CLIENT_ID = "lori.robotmock";
+  public static async Task Main(string[] args)
   {
-    Console.WriteLine("Hello, blah!");
+    var mqttService = new MqttService(CLIENT_ID, "localhost", 1883);
+    await mqttService.Connect();
+
+    await mqttService.Subscribe($"confirm/{CLIENT_ID}", async callback =>
+    {
+      var response = JsonSerializer.Deserialize<RegistrationResponse>(callback.ApplicationMessage.Payload);
+      if (response != null)
+      {
+        await subscribeWithRobotId(mqttService, response.RobotId);
+      }
+    });
+
+    var registration = new RegistrationRequest() { ClientId = CLIENT_ID };
+    var request = JsonSerializer.Serialize<RegistrationRequest>(registration);
+    await mqttService.Publish("register", request);
+
+    Console.WriteLine("Type Enter to exit...");
+    var input = Console.ReadLine();
+
+    Console.WriteLine("Disconnecting...");
+    await mqttService.Disconnect();
+    Console.WriteLine("Done");
+  }
+
+  private static async Task subscribeWithRobotId(IMqttService mqttService, int robotId)
+  {
+    await mqttService.Subscribe($"lori/{robotId}/order", callback =>
+    {
+      Console.WriteLine("Order request called!");
+      return Task.CompletedTask;
+    });
+
+    await mqttService.Subscribe($"lori/{robotId}/delivery", callback =>
+    {
+      Console.WriteLine("Delivery request called!");
+      return Task.CompletedTask;
+    });
+
+    await mqttService.Subscribe($"lori/{robotId}/confirm", callback =>
+    {
+      Console.WriteLine("Confirm order request called!");
+      return Task.CompletedTask;
+    });
   }
 }
