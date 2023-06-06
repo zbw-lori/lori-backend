3423d3979568c5c3d8ea7110394bbb5f13d4ab46
dortlii <22558338+dortlii@users.noreply.github.com>
2023-06-06 15:49:09 +0200

    feat: updated OrderService with prio logic

--- src/lori.backend.Infrastructure/Services/OrderService.cs
+++ src/lori.backend.Infrastructure/Services/OrderService.cs
@@ -1,23 +1,57 @@
-﻿namespace lori.backend.Core.Services;
+﻿namespace lori.backend.Infrastructure.Services;
 
 using lori.backend.Core.Models;
-using lori.backend.Infrastructure.Data;
 public class OrderService
 {
-  private readonly LoriDbContext _context;
-
-  public OrderService(LoriDbContext context)
+  public List<Order> CreateOrderPrio(List<Order> orders)
   {
-    _context = context;
-  }
+    // Get all orders sorted by Prio
+    var sortedOrders = SortOrders(orders);
+    
+    // Set delivery time of each order as early as possible while ensuring uniqueness and not earlier than the earliest delivery time or later than the latest delivery time
+    DateTime currentDeliveryTime = orders[0].EarliestDelivery;
+    foreach (var order in sortedOrders)
+    {
+      if (!order.SetDelivery.HasValue)
+      {
+        DateTime deliveryTime = currentDeliveryTime;
+        if (deliveryTime < order.EarliestDelivery)
+        {
+          deliveryTime = order.EarliestDelivery;
+        }
+        else if (deliveryTime > order.LatestDelivery)
+        {
+          deliveryTime = order.LatestDelivery;
+        }
 
-  public List<Order> GetSortedOrders()
-  {
-    var sortedOrders = _context.Orders
-        .OrderBy(o => o.Priority)
-        .ThenBy(o => o.EarliestDelivery)
-        .ToList();
+        order.SetDelivery = deliveryTime;
+        currentDeliveryTime = GetNextUniqueDeliveryTime(deliveryTime, orders);
+      }
+
+      // Console.WriteLine($"Order: {order.Name}, Priority: {order.Priority}, Earliest Delivery Time: {order.EarliestDelivery}, Latest Delivery Time: {order.LatestDelivery}, Set Delivery Time: {order.SetDelivery}");
+    }
 
     return sortedOrders;
+  }
+
+  public static DateTime GetNextUniqueDeliveryTime(DateTime currentDeliveryTime, List<Order> orders)
+  {
+    DateTime nextDeliveryTime = currentDeliveryTime;
+
+    while (orders.Any(order => order.SetDelivery.HasValue && order.SetDelivery.Value == nextDeliveryTime))
+    {
+      nextDeliveryTime = nextDeliveryTime.AddMinutes(15);
+    }
+
+    return nextDeliveryTime;
+  }
+
+  public List<Order> SortOrders(List<Order> orders)
+  {
+ 
+    // Sort orders by priority
+    orders.Sort((order1, order2) => order2.Priority.CompareTo(order1.Priority));
+
+    return orders;
   }
 }
